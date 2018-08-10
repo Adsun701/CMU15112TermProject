b93b7232416773b12d6e8120d95bfeeed47770ba
Andrew Sun <adsun701@gmail.com>
2018-08-10 10:16:38 -0400

    add randomized player characters and updated behavior for the enemies

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Basic Animation Framework
 
 from tkinter import *
+
 import random, copy, math, sys
 
 ####################################
@@ -18,6 +19,8 @@ class Player(object):
         self.cx = cx
         self.cy = cy
         self.r = r
+        self.appearance = PhotoImage(file=str(random.randint(0, 2)) + ".gif")
+        self.healthColor="Green"
 
     def __repr__(self):
         return "%s, level %d, health %d, attack %d" % (self.name, self.level, self.health, self.attack)
@@ -28,8 +31,8 @@ class Player(object):
     def __eq__(self, other):
         return isinstance(other, self.__class__.__name__) and self.getHashables() == other.getHashables()
     def draw(self, canvas):
-        canvas.create_oval(self.cx - self.r, self.cy - self.r, self.cx + self.r, self.cy + self.r, fill = "yellow")
-        canvas.create_text(self.cx, self.cy + 5, text=self.health)
+        canvas.create_image(self.cx, self.cy, image=self.appearance)
+        canvas.create_text(self.cx, self.cy + 5, text=self.health, fill=self.healthColor)
     def isCollision(self, other):
         if ((other.cx - other.r <= self.cx <= other.cx + other.r) and
             (other.cy - other.r <= self.cy <= other.cy + other.r)):
@@ -46,8 +49,10 @@ class NPC(object):
         self.cx = cx
         self.cy = cy
         self.r = r
+        self.originalR = r
         self.dead = False
         self.color = color
+        self.appearance = PhotoImage(file="wiseau.gif")
 
     def __repr__(self):
         return "%s, level %d, health %d, attack %d" % (self.name, self.level, self.health, self.attack)
@@ -58,7 +63,7 @@ class NPC(object):
     def __eq__(self, other):
         return isinstance(other, self.__class__.__name__) and self.getHashables() == other.getHashables()
     def draw(self, canvas):
-        canvas.create_oval(self.cx - self.r, self.cy - self.r, self.cx + self.r, self.cy + self.r, fill = self.color)
+        canvas.create_image(self.cx, self.cy, image=self.appearance)
         canvas.create_text(self.cx, self.cy + 5, text=self.health)
     def chasePlayer(self, other):
         if (other.__class__.__name__ == "Player" and other.health > 0):
@@ -70,6 +75,31 @@ class NPC(object):
                 self.cy -= 1
             elif other.cy - other.r > self.cy:
                 self.cy += 1
+    def isCollision(self, other):
+        if ((other.cx - other.r <= self.cx <= other.cx + other.r) and
+            (other.cy - other.r <= self.cy <= other.cy + other.r)):
+            return True
+
+class EnemyAttackOrb(object):
+    def __init__(self, cx, cy, r):
+        self.cx = cx
+        self.cy = cy
+        self.r = r
+        self.usedUp = False
+        # West, East, South, North, Northwest, Northeast, Southwest, Southeast
+        self.direction = ([(1, 0), (-1, 0), (0, 1), (0, -1),
+        (-1, -1), (1, -1), (-1, 1), (1, 1)][random.randint(0, 7)])
+
+    def __repr__(self):
+        return "an attack orb from the enemy"
+    def getHashables(self):
+        return (self.cx, self.cy, self.r)
+    def __hash__(self):
+        hash(self.getHashables())
+    def __eq__(self, other):
+        return isinstance(other, self.__class__.__name__) and self.getHashables() == other.getHashables()
+    def draw(self, canvas):
+        canvas.create_oval(self.cx - self.r, self.cy - self.r, self.cx + self.r, self.cy + self.r, fill = "Red")
     def isCollision(self, other):
         if ((other.cx - other.r <= self.cx <= other.cx + other.r) and
             (other.cy - other.r <= self.cy <= other.cy + other.r)):
@@ -118,6 +148,7 @@ class HealingOrb(object):
         self.cy = cy
         self.r = r
         self.usedUp = False
+        self.appearance = PhotoImage(file="spinach" + ".gif")
 
     def __repr__(self):
         return "%s, level %d, health %d, healing %d" % (self.name, self.level, self.health, self.healing)
@@ -128,22 +159,23 @@ class HealingOrb(object):
     def __eq__(self, other):
         return isinstance(other, self.__class__.__name__) and self.getHashables() == other.getHashables()
     def draw(self, canvas):
-        canvas.create_oval(self.cx - self.r, self.cy - self.r, self.cx + self.r, self.cy + self.r, fill = "Cyan")
+        canvas.create_image(self.cx, self.cy, image=self.appearance)
         canvas.create_text(self.cx, self.cy + 5, text=self.health)
 
 def init(data):
-    data.Emma = Player("Emma", "Female", 1, 50, 2, data.width // 2, data.height // 2, 50)
+    data.Emilia = Player("Emilia", "Female", 1, 50, 2, data.width // 2, data.height // 2, 50)
     data.enemyList = []
+    data.enemyAttackOrbList = []
     data.healingOrbList = []
     data.attackOrbList = []
     data.timerCall = 0
     data.gameOver = False
-    data.score = 100000
+    data.score = 0
 
 def mousePressedAction(event, data):
     # use event.x and event.y
     for enemy in data.enemyList:
-        enemy.health -= data.Emma.attack
+        enemy.health -= data.Emilia.attack
 
 def mousePressed(event, data):
     if data.gameOver == False:
@@ -152,35 +184,52 @@ def mousePressed(event, data):
 def keyPressedAction(event, data):
     # Move, wrap around when out of bounds
     if event.keysym == "Left":
-        data.Emma.cx -= 5
-        if data.Emma.cx < 0:
-            data.Emma.cx = data.width - data.Emma.r * 2
+        data.Emilia.cx -= 5
+        if data.Emilia.cx < 0:
+            data.Emilia.cx = data.width - data.Emilia.r * 2
     elif event.keysym == "Right":
-        data.Emma.cx += 5
-        if data.Emma.cx > data.width:
-            data.Emma.cx = data.Emma.r * 2
+        data.Emilia.cx += 5
+        if data.Emilia.cx > data.width:
+            data.Emilia.cx = data.Emilia.r * 2
     elif event.keysym == "Down":
-        data.Emma.cy += 5
-        if data.Emma.cy > data.height:
-            data.Emma.cy = data.Emma.r * 2
+        data.Emilia.cy += 5
+        if data.Emilia.cy > data.height:
+            data.Emilia.cy = data.Emilia.r * 2
     elif event.keysym == "Up":
-        data.Emma.cy -= 5
-        if data.Emma.cy < 0:
-            data.Emma.cy = data.height - data.Emma.r * 2
-    # fire only when there are enemies and that there are less than 10 attack orbs at the time.
-    elif event.keysym == "space" and data.enemyList != list() and len(data.attackOrbList) < 10:
-        data.attackOrbList.append(AttackOrb(data.Emma.cx, data.Emma.cy, 5))
+        data.Emilia.cy -= 5
+        if data.Emilia.cy < 0:
+            data.Emilia.cy = data.height - data.Emilia.r * 2
+    # fire only when there are enemies.
+    elif event.keysym == "space" and data.enemyList != list():
+        data.attackOrbList.append(AttackOrb(data.Emilia.cx, data.Emilia.cy, 5))
         
 
 def keyPressed(event, data):
     if data.gameOver == False:
         keyPressedAction(event, data)
 
+def ShooterEnemies(data):
+    for enemy in data.enemyList:
+        x = random.randint(0, 100)
+        if x % 20 == 0:
+            data.enemyAttackOrbList.append(EnemyAttackOrb(enemy.cx, enemy.cy, 5))
+
+def moveEnemyAttackOrbs(enemyAttackOrb):
+    # move enemyAttackOrbs based on random direction
+    enemyAttackOrb.cx += enemyAttackOrb.direction[0] * 5
+    enemyAttackOrb.cy += enemyAttackOrb.direction[1] * 5
+
+def outOfBoundsEnemyAttackOrb(data, enemyAttackOrb):
+    if enemyAttackOrb.cx < 0 or enemyAttackOrb.cx > data.width:
+        enemyAttackOrb.usedUp = True
+    if enemyAttackOrb.cy < 0 or enemyAttackOrb.cy > data.height:
+        enemyAttackOrb.usedUp = True
+
 def timerFiredAction(data):
     data.timerCall += 1
     if data.timerCall % 500 == 0:
-        if data.score >= 10000:
-            data.enemyList.append(NPC("Chris-R", "Male", 5, random.randint(200, 1000), 10, random.randint(0, data.width), random.randint(0, data.height), 50, "Gray"))
+        if data.score >= 1500:
+            data.enemyList.append(NPC("Chris-R", "Male", 5, random.randint(200, 1000), 10, random.randint(0, data.width), random.randint(0, data.height), 50))
             data.score += 250
         else:
             data.enemyList.append(NPC("Wiseau", "Male", 5, random.randint(20, 500), 10, random.randint(0, data.width), random.randint(0, data.height), 50))
@@ -190,18 +239,18 @@ def timerFiredAction(data):
         data.healingOrbList.append(HealingOrb("Danielle", "Female", 50, 100, 10, random.randint(0, data.width), random.randint(0, data.height), 50))
     # any enemy should chase the player if she is close to it.
     for enemy in data.enemyList:
-        enemy.chasePlayer(data.Emma)
+        enemy.chasePlayer(data.Emilia)
         # if enemy runs out of health,
         # it dies and is removed from the list.
         if enemy.health <= 0:
             enemy.dead = True
             data.score += enemy.maxHealth
         # the player will be damaged if she is too close to an enemy.
-        if data.Emma.isCollision(enemy):
-            if data.timerCall % 5 == 0: data.Emma.health -= 1
+        if data.Emilia.isCollision(enemy):
+            if data.timerCall % 5 == 0: data.Emilia.health -= 1
             # the player dies if she runs out of health,
             # resulting in a game over.
-            if data.Emma.health <= 0:
+            if data.Emilia.health <= 0:
                 data.gameOver = True
     # list comprehension for removing dead enemies
     data.enemyList = [enemy for enemy in data.enemyList if enemy.dead == False]
@@ -211,10 +260,10 @@ def timerFiredAction(data):
     for orb in data.healingOrbList:
         if orb.health <= 0:
             orb.usedUp = True
-        if data.Emma.isCollision(orb):
-            if data.Emma.health < 50:
+        if data.Emilia.isCollision(orb):
+            if data.Emilia.health < data.Emilia.maxHealth:
                 if data.timerCall % 5 == 0:
-                    data.Emma.health += 1
+                    data.Emilia.health += 1
                     orb.health -= 1
     # remove used-up orbs
     data.healingOrbList = [orb for orb in data.healingOrbList if orb.usedUp == False]
@@ -222,13 +271,17 @@ def timerFiredAction(data):
     for attackOrb in data.attackOrbList:
         for enemy in data.enemyList:
             if attackOrb.isCollision(enemy):
-                enemy.health -= data.Emma.attack
+                enemy.health -= data.Emilia.attack
                 attackOrb.usedUp = True
     # list comprehension for removing used-up attack orbs
     data.attackOrbList = [attackOrb for attackOrb in data.attackOrbList if attackOrb.usedUp == False]
     # remove attack orbs if nothing to attack
     if data.enemyList == list():
         data.attackOrbList = list()
+    
+    # list comprehension for removing used-up enemy attack orbs
+    data.enemyAttackOrbList = [enemyAttackOrb for enemyAttackOrb in data.enemyAttackOrbList if enemyAttackOrb.usedUp == False]
+    
     # get indices of enemy list. attack nearest enemy from indices.
     for attackOrb in data.attackOrbList:
         minEnemyDistance = None
@@ -240,34 +293,40 @@ def timerFiredAction(data):
                 closestEnemyIndex = enemyIndex
         attackOrb.homeIn(data.enemyList[closestEnemyIndex])
     
-    ### More difficulty: Gray enemies!
-    for enemyIndex in range(len(data.enemyList)):
-        for otherEnemyIndex in range(len(data.enemyList)):
-            if enemyIndex == otherEnemyIndex:
-                continue
-            elif data.enemyList[enemyIndex].isCollision(data.enemyList[otherEnemyIndex]):
-                if data.timerCall % 5 == 0:
-                    data.enemyList[enemyIndex].r += 1
-                if data.timerCall % 500 == 0:
-                    # INCREASING HEALTH!
-                    data.enemyList[enemyIndex].health += 20
-        
+    ### More difficulty: Enemies shoot projectiles in four cardinal directions!
+    if data.score >= 1500:
+        ShooterEnemies(data)
+    for enemyAttackOrb in data.enemyAttackOrbList:
+        # move orbs
+        moveEnemyAttackOrbs(enemyAttackOrb)
+        if enemyAttackOrb.isCollision(data.Emilia):
+            # damage the player
+            data.Emilia.health -= 1
+            # orb is now used up
+            enemyAttackOrb.usedUp = True
+    
+    # health text is different color depending on health percentage
+    if data.Emilia.maxHealth // 4 < data.Emilia.health <= data.Emilia.maxHealth // 2:
+        data.Emilia.healthColor = "Yellow"
+    elif data.Emilia.health <= data.Emilia.maxHealth // 4:
+        data.Emilia.healthColor = "Red"
+    else:
+        data.Emilia.healthColor = "Green"
 
 def timerFired(data):
     # keep going until player dies
     if data.gameOver == False:
         timerFiredAction(data)
         
-
 def gameOverScreen(canvas, data):
     canvas.create_text(data.width // 2, data.height // 2, anchor="s", text="You died. Final Score: %d" % (data.score), fill="Red", font="Helvetica 56 bold")
 
 def redrawAll(canvas, data):
     # draw in canvas
-    canvas.create_rectangle(0, 0, data.width, data.height, fill = "green", width=0)
+    canvas.create_rectangle(0, 0, data.width, data.height, fill = "black", width=0)
     # if game is not over
     if data.gameOver == False:
-        data.Emma.draw(canvas)
+        data.Emilia.draw(canvas)
     else:
         # game is over
         gameOverScreen(canvas, data)
@@ -277,6 +336,8 @@ def redrawAll(canvas, data):
         orb.draw(canvas)
     for attackOrb in data.attackOrbList:
         attackOrb.draw(canvas)
+    for enemyOrb in data.enemyAttackOrbList:
+        enemyOrb.draw(canvas)
     canvas.create_text(300, 300, text=data.score, fill="Magenta", font="Helvetica 56 bold")
 
 ####################################
